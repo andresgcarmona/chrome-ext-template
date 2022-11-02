ca1808750cf2a76c76b277f8f5f334bf1e1bb2a7
Andrés G. Carmona <ing.andres.carmona@gmail.com>
2022-11-02 08:15:42 -0600

    Updated vite config to add individual files to the compile step.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: fileURLToPath(new URL('./index.html', import.meta.url)),
+      },
+      output: {
+        entryFileNames: "static/js/[name].js",
+      }
+    }
+  }
 })
